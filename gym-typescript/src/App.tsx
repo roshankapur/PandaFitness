8bcebfce643950397ec3fbc13fc295cc39f30da5
roshankapur <kapurroshan2003@gmail.com>
2024-03-28 16:20:43 -0300

    setup navbar

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@/scenes/navbar";
+
 function App() {
 
   return (
     <>
-      <div className="app">
+      <div className="app bg-gray-20">
+        <Navbar/>
       </div>
+      
     </>
   )
 }
