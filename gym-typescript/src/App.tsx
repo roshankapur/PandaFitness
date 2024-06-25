a35bb8b7f435c6304eb04eaa76980ec14f1eb118
Roshan Kapur <kapurroshan2003@gmail.com>
2024-06-25 20:36:48 -0300

    ignore

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,16 +1,36 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import Home from "@/scenes/home";
 
 function App() {
 
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isPageTop, setIsPageTop] = useState<boolean>(true);
 
+  useEffect(() => {
+    const handleScroll = ()=>{
+      if(window.scrollY === 0){
+        setIsPageTop(true);
+        setSelectedPage(SelectedPage.Home);
+      }else{
+        setIsPageTop(false);
+      }
+    }
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    
+  }, [])
   return (
     <>
       <div className="app bg-gray-20">
         <Navbar
+          isPageTop={isPageTop}
           selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+
+        <Home
           setSelectedPage={setSelectedPage}
         />
       </div>
