05fce1dbfff9705762b1ccd3461d20ac10273932
Roshan Kapur <kapurroshan2003@gmail.com>
2024-06-26 01:05:43 -0300

    Created benefits section

--- gym-typescript/src/scenes/home/index.tsx
+++ gym-typescript/src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from '@/assets/SponsorRedBull.png'
 import SponsorForbes from '@/assets/SponsorForbes.png'
 import SponsorFortune from '@/assets/SponsorFortune.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { motion } from 'framer-motion'
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
@@ -20,13 +21,24 @@ const Home = ({setSelectedPage}: Props) => {
         className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
     >
         {/* main header section */}
-        <div 
+        <motion.div 
             className='md:flex mx-auto w-5/6 items-center justify-center md:5/6'
+            onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
         >
             {/*header*/}
             <div className='z-10 mt-32 md:basis-3/5'>
                 {/* headings */}
-                <div className='md:-mt-20'>
+                <motion.div 
+                    className='md:-mt-20'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{once: false, amount: 0.5}}
+                    transition={{duration: 0.5}}
+                    variants={{
+                        hidden: {opacity: 0, x: -50},
+                        visible: {opacity: 1, x: 0}
+                    }}
+                >
                     <div className="relative">
                         <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                             <img alt="home-page-text" src={HomePageText} />
@@ -38,10 +50,20 @@ const Home = ({setSelectedPage}: Props) => {
                         we redefine what it means to reach your peak. 
                         Experience the unrivaled excellence of Evogym – where there's simply no room for excuses.
                     </p>
-                </div>
+                </motion.div>
 
                 {/* call to action */}
-                <div className='mt-8 flex items-center gap-8'>
+                <motion.div 
+                    className='mt-8 flex items-center gap-8'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{once: false, amount: 0.5}}
+                    transition={{delay:0.2, duration: 0.5}}
+                    variants={{
+                        hidden: {opacity: 0, x: -50},
+                        visible: {opacity: 1, x: 0}
+                    }}
+                >
                     <YellowActionButton setSelectedPage={setSelectedPage}>
                         Join Now!
                     </YellowActionButton>
@@ -52,14 +74,14 @@ const Home = ({setSelectedPage}: Props) => {
                     >
                         <p>Learn More</p>
                     </AnchorLink>
-                </div>
+                </motion.div>
             </div>
             {/* title image */}
             <div className='flex basis-3/5 justify-center md:z-10
                     md:ml-40 md:mt-16 md:justify-end'>
                 <img src={HomePageGraphic} alt="homepg graphic"/>
             </div>
-        </div>
+        </motion.div>
 
         {/* sponsor logos */}
         {isMediumLargeScreen && (
